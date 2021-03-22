2f75a934c398080042fec3f489ea298b461134c7
ahmedmeshref <a.meshref@alustudent.com>
2021-03-22 19:14:42 +0200

    run migrations && fix db factory col names

--- database/factories/roomfactory.php
+++ database/factories/roomfactory.php
@@ -7,9 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(room::class, function (Faker $faker) {
     return [
-        'num-beds' => $faker->number_format,
-        'rooms-nums' =>$faker->number_format,
-        'max-num-hosts' =>$faker->sentence(1), // password
+        'num_beds' => $faker->number_format,
+        'rooms_nums' =>$faker->number_format,
+        'max_num_hosts' =>$faker->sentence(1), // password
         'price' =>$faker->sentence(1),
         'admin_id' => factory(App\User::class),
         'type_id' => factory(App\User::class)
